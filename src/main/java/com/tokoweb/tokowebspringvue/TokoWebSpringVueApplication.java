23e14cd60b834dc0c4e2c64bb728ab5a9ca9be66
ustadho <cak.ust@gmail.com>
2018-11-23 09:42:59 +0700

    commit pertama

---
+++ src/main/java/com/tokoweb/tokowebspringvue/TokoWebSpringVueApplication.java
@@ -0,0 +1,12 @@
+package com.tokoweb.tokowebspringvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoWebSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoWebSpringVueApplication.class, args);
+	}
+}
